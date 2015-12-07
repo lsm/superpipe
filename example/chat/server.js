2e208dccb9153e379d6e88ef5e2218eafa2864a5
Anton Nikolenko <nikolenkoanton92@gmail.com>
2015-12-06 17:12:39 -0800

    add chat style, chat client

--- example/chat/server.js
+++ example/chat/server.js
@@ -1,9 +1,19 @@
-var app = require('express')()
+var express = require('express')
+var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var path = require('path')
+
+app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.send('Hello World')
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+io.on('connection', function(socket) {
+  console.log('user connected')
 })
 
 http.listen(3000, function() {
