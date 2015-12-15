1c90e7afcdc3ee2126ead7d3c060fa9886df2063
lsm <liusenmiao@gmail.com>
2015-12-15 11:50:40 -0800

    Remove semicolon from tests.

--- test/pipeline.test.js
+++ test/pipeline.test.js
@@ -1,37 +1,37 @@
-'use strict';
+'use strict'
 /* globals describe, it */
-var assume = require('assume');
-var SuperPipe = require('../');
-var Pipeline = SuperPipe.Pipeline;
-var Injector = SuperPipe.Injector;
-var EventEmitter = require('events').EventEmitter;
+var assume = require('assume')
+var SuperPipe = require('../')
+var Pipeline = SuperPipe.Pipeline
+var Injector = SuperPipe.Injector
+var EventEmitter = require('events').EventEmitter
 
 
 describe('Pipeline', function() {
 
-  var emitter = new EventEmitter();
-  var injector = new Injector();
+  var emitter = new EventEmitter()
+  var injector = new Injector()
 
 
   describe('#listenTo(emitter, name)', function() {
-    var sp = new SuperPipe(injector);
-    var pipeline = new Pipeline(sp);
+    var sp = new SuperPipe(injector)
+    var pipeline = new Pipeline(sp)
     it('should return an instance of Pipeline', function() {
-      assume(pipeline.listenTo('click')).is.instanceOf(Pipeline);
-    });
+      assume(pipeline.listenTo('click')).is.instanceOf(Pipeline)
+    })
 
     it('should throw if listenFn is not a function', function() {
       assume(function() {
-        pipeline.listenTo({}, 'click');
-      }).throws(/^emitter has no listening funciton "on, addEventListener or addListener"/);
-    });
-  });
+        pipeline.listenTo({}, 'click')
+      }).throws(/^emitter has no listening funciton "on, addEventListener or addListener"/)
+    })
+  })
 
   describe('#listenTo("emitterName", eventName)', function() {
     it('should listen to the event if the named emitter is available', function(done) {
-      var injector = new Injector();
-      var sp = new SuperPipe(injector);
-      var pipeline = new Pipeline(sp);
+      var injector = new Injector()
+      var sp = new SuperPipe(injector)
+      var pipeline = new Pipeline(sp)
 
       var emitter = new EventEmitter()
       sp.setDep('myEmitter', emitter)
@@ -42,62 +42,62 @@ describe('Pipeline', function() {
         .pipe(function(event) {
           assume(event).equals('my event')
           done()
-        });
+        })
 
       emitter.emit('click', 'my event')
-    });
+    })
 
     it('should listen to the event once the named emitter becomes available', function(done) {
-      var injector = new Injector();
-      var sp = new SuperPipe(injector);
-      var pipeline = new Pipeline(sp);
+      var injector = new Injector()
+      var sp = new SuperPipe(injector)
+      var pipeline = new Pipeline(sp)
 
       pipeline
         .listenTo('myEmitter', 'click')
         .pipe(function(event) {
           assume(event).equals('my event')
           done()
-        });
+        })
 
       var emitter = new EventEmitter()
       emitter.emit('click', 'other event')
 
       sp.setDep('myEmitter', emitter)
       emitter.emit('click', 'my event')
-    });
-  });
+    })
+  })
 
   describe('#pipe("name of function in deps")', function() {
-    var emitter = new EventEmitter();
-    var sp = new SuperPipe();
+    var emitter = new EventEmitter()
+    var sp = new SuperPipe()
     var pipeline = sp
-      .listenTo(emitter, 'click');
+      .listenTo(emitter, 'click')
 
     it('should accept string as first argument and get the pipe function use the value of the string and call it', function(done) {
       pipeline
         .pipe('setDep')
         .pipe(function(target) {
-          assume(target).equals('x');
-          done();
-        }, 'target');
+          assume(target).equals('x')
+          done()
+        }, 'target')
       emitter.emit('click', {
         target: 'x'
-      });
-    });
+      })
+    })
 
     it('should accept object as hashed dependencies', function() {
       var hashedDeps = function(obj, fn) {
-        assume(obj.myFn).equals(hashedDeps);
-        assume(fn).equals(hashedDeps);
+        assume(obj.myFn).equals(hashedDeps)
+        assume(fn).equals(hashedDeps)
       }
       sp.setDep('hashedDeps', hashedDeps)
         .listenTo(emitter, 'keydown')
         .pipe('hashedDeps', [{
           myFn: 'hashedDeps'
-        }, 'hashedDeps']);
-      emitter.emit('keydown');
-    });
-  });
+        }, 'hashedDeps'])
+      emitter.emit('keydown')
+    })
+  })
 
   describe('#pipe("emit")', function() {
 
@@ -119,120 +119,120 @@ describe('Pipeline', function() {
         .pipe('emit', 'event 1', ['event', 'obj'])
 
       sp.emit('event 2')
-    });
+    })
 
-  });
+  })
 
   describe('#pipe(number)', function() {
-    var sp = new SuperPipe(injector);
-    var pipeline = sp.listenTo('fast');
+    var sp = new SuperPipe(injector)
+    var pipeline = sp.listenTo('fast')
 
     it('should throttle the events emitted', function(done) {
-      var normalCounter = 0;
-      var throttledCounter = 0;
+      var normalCounter = 0
+      var throttledCounter = 0
       pipeline
         .pipe(function() {
-          normalCounter++;
+          normalCounter++
         })
         .pipe(100)
         .pipe(function() {
-          throttledCounter++;
+          throttledCounter++
           if (throttledCounter === 2) {
-            assume(normalCounter).equals(11);
-            done();
+            assume(normalCounter).equals(11)
+            done()
           }
-        });
+        })
 
       for (var i = 0; i < 10; i++) {
-        sp.emit('fast');
+        sp.emit('fast')
       }
 
       setTimeout(function() {
-        sp.emit('fast');
-      }, 110);
-    });
-  });
+        sp.emit('fast')
+      }, 110)
+    })
+  })
 
   describe('#pipe(function, dependencies)', function() {
     function clickHandler() {
     }
 
-    var value = 1;
+    var value = 1
 
     function processer(handlerFn, setDep) {
-      assume(handlerFn).equals(clickHandler);
-      setDep('valueFromProcesserA', value);
-      value++;
-      return true;
+      assume(handlerFn).equals(clickHandler)
+      setDep('valueFromProcesserA', value)
+      value++
+      return true
     }
 
-    var sp = new SuperPipe(injector);
+    var sp = new SuperPipe(injector)
     var pipeline = sp
       .setDep('handleClick', clickHandler)
-      .listenTo(emitter, 'click');
+      .listenTo(emitter, 'click')
 
     it('should throw if fn is not a function or name of a dependency function', function() {
       assume(function() {
-        pipeline.pipe({});
-      }).throws(/^fn should be a function, got/);
-    });
+        pipeline.pipe({})
+      }).throws(/^fn should be a function, got/)
+    })
 
     it('should accept mutiple arguments string as dependencies', function() {
-      pipeline.pipe(processer, ['handleClick', 'setDep']);
-      emitter.emit('click');
-    });
+      pipeline.pipe(processer, ['handleClick', 'setDep'])
+      emitter.emit('click')
+    })
 
     it('should accept array as dependencies', function() {
-      pipeline.pipe(processer, ['handleClick', 'setDep']);
-      emitter.emit('click');
-    });
+      pipeline.pipe(processer, ['handleClick', 'setDep'])
+      emitter.emit('click')
+    })
 
     it('should has 0 or null as context for each pipeline functions', function() {
       pipeline.pipe(function() {
         assume(+this).equals(0)
-      });
-    });
+      })
+    })
 
     it('should keep the original argument if null is provided while still provide the right dependency need to be injected', function() {
       pipeline
         .pipe(function(event, next) {
-          assume(event.target).equals('a');
-          next();
-        }, [null, 'next']);
+          assume(event.target).equals('a')
+          next()
+        }, [null, 'next'])
       emitter.emit('click', {
         target: 'a'
-      });
+      })
 
-    });
+    })
 
     it('should not register getDep as a dependency', function() {
       pipeline.pipe(function(getDep) {
-        assume(getDep).equals(undefined);
-      }, 'getDep');
-    });
+        assume(getDep).equals(undefined)
+      }, 'getDep')
+    })
 
     it('should register setDep as dependencies and bind with dependencies of current pipeline', function(done) {
       pipeline
         .pipe(function(setDep) {
-          assume(sp.setDep).not.equals(setDep);
-          sp.setDep('key', 'old value');
-          setDep('key', 'new value');
+          assume(sp.setDep).not.equals(setDep)
+          sp.setDep('key', 'old value')
+          setDep('key', 'new value')
         }, 'setDep')
         .pipe(function(key) {
-          assume(key).equals('new value');
-          assume(sp.getDep('key')).equals('old value');
-          done();
-        }, 'key');
+          assume(key).equals('new value')
+          assume(sp.getDep('key')).equals('old value')
+          done()
+        }, 'key')
 
       emitter.emit('click', {
         target: 'a'
-      });
-    });
+      })
+    })
 
     it('should call the piped stream functions for the correct times', function() {
-      assume(value).equals(8);
-    });
-  });
+      assume(value).equals(8)
+    })
+  })
 
   describe('#pipe(function, dependencies, supplies)', function() {
     var superpipe = new SuperPipe(injector)
@@ -311,7 +311,7 @@ describe('Pipeline', function() {
 
   describe('#pipe(function, [3, "dep1", "dep2"])', function() {
     it('should skip dependencies by number and get the default values by position.', function() {
-      var sp = new SuperPipe();
+      var sp = new SuperPipe()
       sp.setDep('dep1', 'value1')
       sp.setDep('dep2', 'value2')
 
@@ -334,36 +334,36 @@ describe('Pipeline', function() {
   })
 
   describe('#error', function() {
-    var sp = new SuperPipe(injector);
-    sp.setDep('dep1', 1);
-    var pipeline = sp.listenTo(emitter, 'normal');
-    var errMessage = 'Error message';
-    var normalMessage = 'Normal message';
+    var sp = new SuperPipe(injector)
+    sp.setDep('dep1', 1)
+    var pipeline = sp.listenTo(emitter, 'normal')
+    var errMessage = 'Error message'
+    var normalMessage = 'Normal message'
 
     it('should call error handler when next is called with truthy first argument', function(done) {
       pipeline
         .pipe(function(msg) {
-          assume(msg).equals(normalMessage);
+          assume(msg).equals(normalMessage)
         })
         .pipe(function(next) {
-          next(errMessage);
-        }, 'next');
+          next(errMessage)
+        }, 'next')
       pipeline.error(function(err) {
-        assume(err).equals(errMessage);
-        done();
-      });
-      emitter.emit('normal', normalMessage);
-    });
+        assume(err).equals(errMessage)
+        done()
+      })
+      emitter.emit('normal', normalMessage)
+    })
 
     it('should call error handler with requested dependencies', function(done) {
       pipeline.error(function(err, arg1) {
-        assume(err).equals(errMessage);
-        assume(arg1).equals(1);
-        done();
-      }, [null, 'dep1']);
-      emitter.emit('normal', normalMessage);
-    });
-  });
+        assume(err).equals(errMessage)
+        assume(arg1).equals(1)
+        done()
+      }, [null, 'dep1'])
+      emitter.emit('normal', normalMessage)
+    })
+  })
 
   describe('events', function() {
     var sp = new SuperPipe()
@@ -371,15 +371,15 @@ describe('Pipeline', function() {
       sp
         .listenTo('eventA')
         .pipe(function(emit) {
-          emit('eventB', 'data of eventB');
+          emit('eventB', 'data of eventB')
         }, 'emit')
       sp
         .listenTo('eventB')
         .pipe(function(data) {
-          assume(data).equals('data of eventB');
+          assume(data).equals('data of eventB')
           done()
         })
       sp.emit('eventA')
-    });
-  });
-});
+    })
+  })
+})
--- test/superpipe.test.js
+++ test/superpipe.test.js
@@ -1,7 +1,7 @@
 /* globals describe, beforeEach, it*/
-var SuperPipe = require('../');
+var SuperPipe = require('../')
 var assume = require('assume')
-var Pipeline = SuperPipe.Pipeline;
+var Pipeline = SuperPipe.Pipeline
 var forEach = require('lodash.foreach')
 
 
@@ -9,24 +9,24 @@ describe('SuperPipe', function() {
 
   describe('#constructor', function() {
     it('should return an instance of SuperPipe', function() {
-      var sp = new SuperPipe();
-      assume(sp).is.instanceOf(SuperPipe);
-      assume(sp.listenTo).is.a('function');
-      assume(sp.setDep).is.a('function');
-      assume(sp.getDep).is.a('function');
-    });
+      var sp = new SuperPipe()
+      assume(sp).is.instanceOf(SuperPipe)
+      assume(sp.listenTo).is.a('function')
+      assume(sp.setDep).is.a('function')
+      assume(sp.getDep).is.a('function')
+    })
 
     it('should register function members as dependencies', function() {
-      var sp = new SuperPipe();
-      assume(sp.autoBind).is.a('function');
-      assume(sp.isDepNameReserved).is.a('function');
-    });
-  });
+      var sp = new SuperPipe()
+      assume(sp.autoBind).is.a('function')
+      assume(sp.isDepNameReserved).is.a('function')
+    })
+  })
 
   describe('#setDep/#getDep', function() {
-    var sp;
-    var depStr = 'example';
-    var DepClass = function() {};
+    var sp
+    var depStr = 'example'
+    var DepClass = function() {}
 
     DepClass.prototype = {
       x: 1,
@@ -36,156 +36,156 @@ describe('SuperPipe', function() {
       fn2: function fn2() {},
       _x: 10,
       _fn: function() {}
-    };
+    }
 
-    var depObject = new DepClass();
+    var depObject = new DepClass()
 
     beforeEach(function() {
-      sp = new SuperPipe();
-    });
+      sp = new SuperPipe()
+    })
 
     it('should throw if arguments signature is not recognized', function() {
       assume(function() {
-        sp.setDep();
-      }).throws(/^Unsupported function arguments/);
+        sp.setDep()
+      }).throws(/^Unsupported function arguments/)
       assume(function() {
-        sp.setDep(function() {});
-      }).throws(/^Unsupported function arguments/);
-    });
+        sp.setDep(function() {})
+      }).throws(/^Unsupported function arguments/)
+    })
 
     it('should be okay to set falsity values as dependencies', function() {
-      sp.setDep('name', '');
-      assume(sp.getDep('name')).equals('');
-      sp.setDep('name');
-      assume(sp.getDep('name')).equals(undefined);
-      sp.setDep('name', null);
-      assume(sp.getDep('name')).equals(null);
-      sp.setDep('name', false);
-      assume(sp.getDep('name')).equals(false);
-      sp.setDep('name', 0);
-      assume(sp.getDep('name')).equals(0);
-    });
+      sp.setDep('name', '')
+      assume(sp.getDep('name')).equals('')
+      sp.setDep('name')
+      assume(sp.getDep('name')).equals(undefined)
+      sp.setDep('name', null)
+      assume(sp.getDep('name')).equals(null)
+      sp.setDep('name', false)
+      assume(sp.getDep('name')).equals(false)
+      sp.setDep('name', 0)
+      assume(sp.getDep('name')).equals(0)
+    })
 
     it('should throw if set dependency with reserved name', function() {
       assume(function() {
         sp.setDep({
           setDep: 1
-        });
-      }).throws(/The name of your dependency is reserved:/);
+        })
+      }).throws(/The name of your dependency is reserved:/)
       forEach(sp.reservedDeps, function(reserved) {
         assume(function() {
-          sp.setDep(reserved, {});
-        }).throws(/The name of your dependency is reserved:/);
-      });
-    });
+          sp.setDep(reserved, {})
+        }).throws(/The name of your dependency is reserved:/)
+      })
+    })
 
     it('should set dependency with name and value', function() {
-      sp.setDep('dep1', depObject);
-      assume(depObject).deep.equals(sp.getDep('dep1'));
-    });
+      sp.setDep('dep1', depObject)
+      assume(depObject).deep.equals(sp.getDep('dep1'))
+    })
 
     it('should override old dependency with new value', function() {
-      sp.setDep('dep1', depObject);
-      sp.setDep('dep1', depStr);
-      assume(sp.getDep('dep1')).equals(depStr);
-    });
+      sp.setDep('dep1', depObject)
+      sp.setDep('dep1', depStr)
+      assume(sp.getDep('dep1')).equals(depStr)
+    })
 
     it('should accept object and add all properties as dependencies', function() {
       var obj = {
         a: 'a',
         b: 2
-      };
-      sp.setDep(obj);
-      assume(sp.getDep('a')).equals(obj.a);
-      assume(sp.getDep('b')).equals(obj.b);
-    });
+      }
+      sp.setDep(obj)
+      assume(sp.getDep('a')).equals(obj.a)
+      assume(sp.getDep('b')).equals(obj.b)
+    })
 
     it('should add all properties except functions as dependencies if props is *$', function() {
-      sp.setDep(depObject, '*$');
-      assume(sp.getDep('x')).equals(depObject.x);
-      assume(sp.getDep('y')).equals(depObject.y);
-      assume(sp.getDep('z')).equals(depObject.z);
-      assume(sp.getDep('fn1')).is.not.exist();
-      assume(sp.getDep('fn2')).is.not.exist();
-    });
+      sp.setDep(depObject, '*$')
+      assume(sp.getDep('x')).equals(depObject.x)
+      assume(sp.getDep('y')).equals(depObject.y)
+      assume(sp.getDep('z')).equals(depObject.z)
+      assume(sp.getDep('fn1')).is.not.exist()
+      assume(sp.getDep('fn2')).is.not.exist()
+    })
 
     it('should add all functions as dependencies if props is *^', function() {
-      sp.setDep(depObject, '*^');
-      assume(sp.getDep('fn1')).equals(depObject.fn1);
-      assume(sp.getDep('fn2')).equals(depObject.fn2);
-      assume(sp.getDep('x')).is.not.exist();
-      assume(sp.getDep('y')).is.not.exist();
-      assume(sp.getDep('z')).is.not.exist();
-    });
+      sp.setDep(depObject, '*^')
+      assume(sp.getDep('fn1')).equals(depObject.fn1)
+      assume(sp.getDep('fn2')).equals(depObject.fn2)
+      assume(sp.getDep('x')).is.not.exist()
+      assume(sp.getDep('y')).is.not.exist()
+      assume(sp.getDep('z')).is.not.exist()
+    })
 
     it('should use name as prefix', function() {
-      sp.setDep('prefix', depObject, '*^');
-      assume(sp.getDep('prefix:fn1')).equals(depObject.fn1);
-      assume(sp.getDep('prefix:fn2')).equals(depObject.fn2);
-      assume(sp.getDep('prefix:x')).is.not.exist();
-      assume(sp.getDep('prefix:y')).is.not.exist();
-      assume(sp.getDep('prefix:z')).is.not.exist();
-    });
+      sp.setDep('prefix', depObject, '*^')
+      assume(sp.getDep('prefix:fn1')).equals(depObject.fn1)
+      assume(sp.getDep('prefix:fn2')).equals(depObject.fn2)
+      assume(sp.getDep('prefix:x')).is.not.exist()
+      assume(sp.getDep('prefix:y')).is.not.exist()
+      assume(sp.getDep('prefix:z')).is.not.exist()
+    })
 
     it('should add all properties as dependencies if props is *', function() {
-      sp.setDep('name', depObject, '*');
-      assume(sp.getDep('name:x')).equals(depObject.x);
-      assume(sp.getDep('name:y')).equals(depObject.y);
-      assume(sp.getDep('name:z')).equals(depObject.z);
-      assume(sp.getDep('name:fn1')).equals(depObject.fn1);
-      assume(sp.getDep('name:fn2')).equals(depObject.fn2);
-    });
+      sp.setDep('name', depObject, '*')
+      assume(sp.getDep('name:x')).equals(depObject.x)
+      assume(sp.getDep('name:y')).equals(depObject.y)
+      assume(sp.getDep('name:z')).equals(depObject.z)
+      assume(sp.getDep('name:fn1')).equals(depObject.fn1)
+      assume(sp.getDep('name:fn2')).equals(depObject.fn2)
+    })
 
     it('should not register dependencies start with _ when props is any of */*$/*^', function() {
-      sp.setDep('name', depObject, '*');
-      assume(sp.getDep('_x')).equals(undefined);
-      assume(sp.getDep('_fn')).equals(undefined);
+      sp.setDep('name', depObject, '*')
+      assume(sp.getDep('_x')).equals(undefined)
+      assume(sp.getDep('_fn')).equals(undefined)
 
-      sp = new SuperPipe();
-      sp.setDep('name', depObject, '*^');
-      assume(sp.getDep('_fn')).equals(undefined);
+      sp = new SuperPipe()
+      sp.setDep('name', depObject, '*^')
+      assume(sp.getDep('_fn')).equals(undefined)
 
-      sp = new SuperPipe();
-      sp.setDep('name', depObject, '*$');
-      assume(sp.getDep('_x')).equals(undefined);
-    });
-  });
+      sp = new SuperPipe()
+      sp.setDep('name', depObject, '*$')
+      assume(sp.getDep('_x')).equals(undefined)
+    })
+  })
 
   describe('#autoBind', function() {
     it('should bind the right context to function dependencies when autoBind is true', function(done) {
-      var sp = new SuperPipe();
+      var sp = new SuperPipe()
       var context = {
         fn1: function() {
-          assume(this).equals(context);
-          this.fn1Called = true;
+          assume(this).equals(context)
+          this.fn1Called = true
         },
         fn2: function() {
-          assume(context.fn1Called).equals(true);
-          assume(this).is.not.equal(context);
-          done();
+          assume(context.fn1Called).equals(true)
+          assume(this).is.not.equal(context)
+          done()
         }
-      };
-      sp.autoBind(true);
-      sp.setDep(context, '*^');
-      sp.getDep('fn1').call({});
+      }
+      sp.autoBind(true)
+      sp.setDep(context, '*^')
+      sp.getDep('fn1').call({})
 
-      sp.autoBind(false);
-      sp.setDep(context, '*^');
-      sp.getDep('fn2').call({});
-    });
-  });
+      sp.autoBind(false)
+      sp.setDep(context, '*^')
+      sp.getDep('fn2').call({})
+    })
+  })
 
   describe('#pipeline', function() {
-    var sp = new SuperPipe();
+    var sp = new SuperPipe()
     it('should return an instance of Pipeline', function() {
-      assume(sp.pipeline()).is.instanceOf(Pipeline);
-    });
-  });
+      assume(sp.pipeline()).is.instanceOf(Pipeline)
+    })
+  })
 
   describe('#listenTo', function() {
-    var sp = new SuperPipe();
+    var sp = new SuperPipe()
     it('should return an instance of Pipeline', function() {
-      assume(sp.listenTo('click')).is.instanceOf(Pipeline);
-    });
-  });
-});
+      assume(sp.listenTo('click')).is.instanceOf(Pipeline)
+    })
+  })
+})
