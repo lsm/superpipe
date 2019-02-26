d23355f8749a4f2f7e0191f99fcf084041b1d49b
Marc <marc@primoinv.com>
2019-02-26 15:10:18 -0800

    Add rollup config.

---
+++ rollup.config.js
@@ -0,0 +1,59 @@
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import { uglify } from 'rollup-plugin-uglify'
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+const NODE_ENV = process.env.NODE_ENV
+
+const config = {
+  input: 'src/index.js',
+  output: {
+    format: 'umd',
+    name: 'Superpipe'
+  },
+
+  plugins: [
+    nodeResolve(),
+    babel({
+      babelrc: false,
+      presets: [
+        [
+          'env',
+          {
+            targets: {
+              browsers: [
+                'last 1 version',
+                '> 1%',
+                'maintained node versions',
+                'not dead'
+              ]
+            },
+            modules: false
+          }
+        ]
+      ],
+      plugins: ['external-helpers', 'transform-object-rest-spread'],
+      exclude: '**/node_modules/**'
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+    }),
+    commonjs()
+  ]
+}
+
+if (NODE_ENV === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+export default config
