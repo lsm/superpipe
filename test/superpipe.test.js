f917c9a0c62d4a187762a726b4e831bbda96fe88
Marc Liu <m@liu.pm>
2019-03-06 14:45:21 -0800

    Rewrite pipeline constructor.

--- test/superpipe.test.js
+++ test/superpipe.test.js
@@ -1,10 +1,10 @@
-/* globals describe, it */
 import { expect } from 'chai'
+import { describe, it } from 'mocha'
 import superpipe from '../src'
 
-describe('Superpipe', function() {
-  describe('superpipe()', function() {
-    it('should create pipeline constructor', function() {
+describe('Superpipe', function () {
+  describe('superpipe()', function () {
+    it('should create pipeline constructor', function () {
       let sp = superpipe()
       expect(sp).to.be.a('function')
 
@@ -16,19 +16,84 @@ describe('Superpipe', function() {
     })
   })
 
-  describe('superpipe(deps)', function() {
-    it('should use `deps` as dependencies when creating pipeline', () => {
-      let deps = { key: 'value' }
-      var sp = superpipe(deps)
-      let pipeline = sp('key value pipeline')
-        .pipe(
-          key => {
-            expect(key).to.equal(deps.key)
-          },
-          'key'
-        )
+  describe('superpipe(functions)', function () {
+    it('should use `functions` as dependencies when creating pipeline', (done) => {
+      const functions = {
+        func (key) {
+          expect(key).to.equal('value')
+          done()
+        }
+      }
+      var sp = superpipe(functions)
+      const pipeline = sp('key value pipeline')
+        .input('key')
+        .pipe('func', 'key')
         .end()
-      pipeline()
+      pipeline('value')
+    })
+
+    it('should create pipeline function directly from definitions', () => {
+      const sp = superpipe()
+      const func = sp('from defs', [
+        ['pipe1', 'arg'],
+        ['end', 'arg']
+      ])
+      expect(func).to.be.a('function')
+
+      const pl = sp('from defs', [
+        ['pipe1', 'arg'],
+        ['pipe2', 'arg']
+      ])
+      expect(pl.pipe).to.be.a('function')
+    })
+  })
+
+  describe('Exceptions', () => {
+    const sp = superpipe()
+
+    it('should throw if input is called not at the first place', () => {
+      expect(() => {
+        sp('mypl')
+          .pipe('abc')
+          .input(['arg'])
+      }).to.throw('Input must be called before any other pipes.')
+
+      expect(() => {
+        sp('mypl', [
+          ['someFunc', 'arg', 'result'],
+          ['input', '{arg}']
+        ])
+      }).to.throw('Input must be called before any other pipes.')
+    })
+
+    it('should throw when adding new pipe after error pipe', () => {
+      expect(() => {
+        sp('mypl')
+          .error('abc')
+          .pipe('arg')
+      }).to.throw('Adding new pipe after error pipe is not allowed.')
+
+      expect(() => {
+        sp('mypl', [
+          ['error', 'errorHandler', 'error input'],
+          ['someFunc', ['arg']]
+        ])
+      }).to.throw('Adding new pipe after error pipe is not allowed.')
+    })
+
+    it('should throw when adding more than one error handler', () => {
+      expect(() => {
+        sp('mypl')
+          .error('abc')
+          .error('arg')
+      }).to.throw('Each pipeline could only have one error handler.')
+
+      expect(() => {
+        sp('mypl', [
+          ['error', 'arg', 'result'],
+          ['error', '{arg}']
+        ])
+      }).to.throw('Each pipeline could only have one error handler.')
     })
   })
 })
